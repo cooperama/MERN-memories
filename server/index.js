5dfd23e144e6910a3e313697cba92e7314a87857
cooperama <coope133@gmail.com>
2020-11-27 16:47:08 -0800

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to memories API");
+});
 
 const PORT = process.env.PORT || 5000;
 
